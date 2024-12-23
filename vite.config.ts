b91914acf06fef1131c7b289da86da7983f47609
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2024-12-23 16:15:48 -0300

    upgrade tailwind to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import ViteRails from 'vite-plugin-rails'
+// @ts-ignore
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     ViteRails(),
+    tailwindcss(),
   ],
 })
